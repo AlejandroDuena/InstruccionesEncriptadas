788e6b412023bff357773cb0497b4bc17448abc0
Dueñas Moreno <kqbb789@astrazeneca.net>
2023-02-02 15:19:40 -0600

    Problema numero 1 Completado.

--- src/InstruccionesEncriptadas.java
+++ src/InstruccionesEncriptadas.java
@@ -1,5 +1,74 @@
-public class Main {
+import java.io.*;
+import java.util.Scanner;
+
+public class InstruccionesEncriptadas {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String Ins = "";
+        File archivo;
+        FileReader fr = null;
+        BufferedReader br = null;
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+
+        try {
+            archivo = new File("instrucciones.txt");
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+            int m1 = 0, m2 = 0;
+            int n = 0;
+            String instruc1 = "";
+            String instruc2 = "";
+            String mensaje = "";
+            sc = new Scanner(archivo);
+
+            m1 = sc.nextInt();
+            m2 = sc.nextInt();
+            n = sc.nextInt();
+            instruc1 = sc.next();
+            instruc2 = sc.next();
+            mensaje = sc.next();
+
+            if ((n >= 3 && n <= 5000) && (m1 >= 2 && m1 <= 50) && (m2 >= 2 && m2 <= 50)) {
+                int i = 0;
+                int j = 0;
+                while (i < mensaje.length() && j < instruc1.length()){
+                    i++;
+                    j++;
+                    if(j == instruc1.length()){
+                        instruc1 = "SI";
+                    }else {
+                        instruc1 = "NO";
+                    }
+                }
+                while(i < mensaje.length() && j < instruc2.length()){
+                    i++;
+                    j++;
+                    if(j == instruc2.length()){
+                        instruc2 = "SI";
+                    }else {
+                        instruc2 = "NO";
+                    }
+                }
+            }
+            try{
+                fichero = new FileWriter("resultados.txt");
+                pw = new PrintWriter(fichero);
+                pw.println(instruc1);
+                pw.println(instruc2);
+            } catch (Exception e){
+                e.printStackTrace();
+            }finally {
+                try{
+                    if(null != fichero);
+                    fichero.close();
+                }catch (Exception e2){
+                    e2.printStackTrace();
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
